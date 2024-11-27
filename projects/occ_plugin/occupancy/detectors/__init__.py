01ccef238b31d8dde5e88123c9b3959c5a21a497
Ericwen2001 <61090824+Ericwen2001@users.noreply.github.com>
2024-11-27 21:23:39 +0800

    Update __init__.py
    bug fix

--- projects/occ_plugin/occupancy/detectors/__init__.py
+++ projects/occ_plugin/occupancy/detectors/__init__.py
@@ -1,16 +1,3 @@
-from .occnet import OccNet
-from .radaroccnet import RadarOccNet
-from .radarocc_attn import RadarOccAttn
-from .radarocc_attn_learn import RadarOccAttn_learn
-from .radar_tensor_occ import tensorOcc
-from .radarocc_polar import RadarTensorAttn
-from .radarocc_polar_doppler import RadarDopplerAttn
-from .radarocc_polar_doppler_EA import RadarDopplerAttnEA
-from .radar_polar_cube import RadarPolarCube
-from .radarocc_polar_4d import RadarPolar4d
 from .radarocc_self import RadarDopplerAttnEASelf
-from .radarocc_cross_first import RadarCart
-from .occnet_voxel import voxelocc
-from .radarocc_self_SS import RadarDopplerAttnEASelfSS
 from .radarocc_self_small import RadarOcc_small
 # from .radarocc_polar_tensor import RadarTensorAttn
